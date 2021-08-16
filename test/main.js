9118a64e1ca369e631a2ca2746155b16be822397
tjomashah <artem10-messi10@ukr.net>
2021-08-16 19:29:54 +0200

    refactoring main file

--- test/main.js
+++ test/main.js
@@ -8,55 +8,33 @@ const opts = {
         platformVersion: "11",
         deviceName: "pixel_4",
         automationName: "UiAutomator2",
+        noReset: true,
         app: "/home/artem/Delinternet/android-test-with-appium/whatsapp.com.apk",
-        //app: "/home/artem/Android/Sdk/platform-tools/whatsapp.com.apk",
         appPackage: "com.whatsapp",
         appActivity: "com.whatsapp.Main", 
-        // app: "/home/artem/Delinternet/Android_test/app-test/messages.com.apk",
-        // appPackage: "com.imessages.messaging.chat",
-        // appActivity: "com.imessages.messaging.chat.sms.activity.SplashActivity",
     }
 };
 
 async function main() {
     const client = await wdio.remote(opts);
+  
+    const searchBtn = await client.$('//android.widget.TextView[@content-desc="Search"]');
+    await searchBtn.click();
 
-    const agreeBtn = await client.$("//android.widget.Button[@index='6']");
-    await agreeBtn.click();
+    const inputField = await client.$('//android.widget.EditText[@resource-id="com.whatsapp:id/search_input"]');
+    await inputField.setValue("Me");
 
-    const codeNum = await client.$("//android.widget.EditText[@index='0']");
-    await codeNum.setValue("34");
+    const applyBtn = await client.$('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout[2]/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.RelativeLayout/android.widget.LinearLayout');
+    await applyBtn.click();
 
-    const mainNum = await client.$("//android.widget.EditText[@index='1']");
-    await mainNum.setValue("621274614");
+    const msgContent = await client.$('//android.widget.EditText[@index="1"]');
+    await msgContent.setValue("Test message");
 
-    const nextBtn = await client.$("//android.widget.Button[@index='4']");
-    await nextBtn.click();
-
-    const okBtn = await client.$("//android.widget.Button[@index='1']");
-    await okBtn.click();
-
-    ////////// sms ////////////////
-    // const checkBtn = await client.$("//android.widget.CheckBox[@index='0']");
-    // await checkBtn.click();
-
-    // const defBtn = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.LinearLayout[2]");
-    // await defBtn.click();
-
-    // const allowBtn = await client.$("//android.widget.Button[@index='0']");
-    // await allowBtn.click();
-
-    // const allowBtnCall = await client.$("//android.widget.Button[@index='0']");
-    // await allowBtnCall.click();
-
-    // const allowBtnMsg = await client.$("//android.widget.Button[@index='0']");
-    // await allowBtnMsg.click();
-
-    // const cancelBtn = await client.$("//android.widget.Button[@index='0']");
-    // driver.pause(10000)
-    // await cancelBtn.click();
-    ////////////////////////////
+    const sendBtn = await client.$('//android.widget.ImageButton[@content-desc="Send"]');
+    await sendBtn.click()
+    
     await client.deleteSession();
+    
 }
 
 main();
